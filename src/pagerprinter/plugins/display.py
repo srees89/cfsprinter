86ffd69c8aa83edf144cdb69e863544ebee3a974
Shaggy <rees.shane@gmail.com>
2016-10-02 23:02:54 +1030

    Made the display easier to read

--- src/pagerprinter/plugins/display.py
+++ src/pagerprinter/plugins/display.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Display Plugin
-Copyright 2015 Shane Rees <https://github.com/Shaggs/>
+Copyright 2016 Shane Rees <https://github.com/Shaggs/>
 
 A Small GUI to display pager message on a screen. Will change from
 Green > orange > Red at a 3 minute interval this way upon entering the
@@ -23,10 +23,35 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 from __future__ import absolute_import
 from . import BasePlugin
 from Tkinter import Tk, BOTH, INSERT, Text
+import re
 
 
 class Display(BasePlugin):
 	def execute(self, msg, unit, address, when, printer, print_copies):
+		trigger = 'RESPOND'
+		trigger_end = 'MAP'
+		addr = msg.split(trigger)[1]
+		incno = msg.split(' ')[2]
+		date = msg.split(' ')[3]
+		time = msg.split(' ')[4]
+		inc = addr.split(',')[0]
+		alarm = addr.split(',')[1]
+		map = addr.split(',')[3]
+		map = re.sub(r'MAP:', '', map)
+		tg = addr.split(',')[4]
+		if "== ==" in addr:
+		 ex = addr.split("== ==")[1]
+		else:
+		 ex = addr.split("==")[1]
+		ex = ex.split(':')[0]
+		sunit = msg.split (':')[6]
+		ex = ex.replace('==' or "== ==", '')
+		tg = re.sub(r'TG', '', tg)
+		tg = tg.strip("  ")
+		addr = addr.split(trigger_end)[0]
+		addr = re.sub(r'#\d{3}/\d{3}|@|\s:\s', '', addr)
+		addr_p = addr.split(',')[-2:]
+		addr = ','.join(addr_p)
 		def orange():
 			text_widget.config(bg="Orange")
 			root.after(180000, red)
@@ -43,10 +68,44 @@ class Display(BasePlugin):
 		text_widget = Text(root, font='times 40 bold', bg='Green')
 		text_widget.pack(fill=BOTH, expand=0)
 		text_widget.tag_configure('tag-center', wrap='word', justify='center')
-		text_widget.insert(INSERT, "\n" + "\n" + "\n" + mseg, 'tag-center')
-		root.after(180000, orange)
+		text_widget.insert(INSERT,''' Date: %(date)s
 
-		# TODO: this needs to spawn a thread
+Time: %(time)s 
+
+Incident Type: %(inc)s  
+
+Incident Number: %(incno)s
+
+Level: %(alarm)s
+
+Message: %(ex)s
+
+Info/Address: %(addr)s
+
+Map Ref: %(map)s
+
+Talk Group: %(tg)s 
+
+Resources: %(sunit)s
+
+Raw:  %(msg)s '''
+		% dict(msg=msg, incno=incno, inc=inc, alarm=alarm, addr=addr, ex=ex, tg=tg, sunit=sunit, map=map, date=date,time=time), 'tag-center')
+		root.after(180000, orange)
 		root.mainloop()
 
 PLUGIN = Display
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
